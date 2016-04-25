8a2af050221a2db62e37cc72db85d4d9992a6cd9
maoosi <hello@sylvainsimao.fr>
2016-04-25 23:44:08 +1000

    Core update

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 ## spritz.js
 A small, modern, dependency-free sprites animation library. Compatible with ScrollMagic.io
+
+## TODO
+[ ] Library v1.0.0
+[ ] Better server sync
+[ ] Eslint
+[ ] Complete demo page
+[ ] Documentation
+[ ] NPM publish
+[ ] JSDelivr
+[ ] Canvas option
--- src/spritz.js
+++ src/spritz.js
@@ -1,6 +1,8 @@
 import knot from 'knot.js'
+import shortid from 'shortid'
 
-export default (target, options = {}) => {
+
+export default (options = {}) => {
 
 	/**
 	 * Default settings
@@ -8,6 +10,7 @@ export default (target, options = {}) => {
 
 	const settings = {
 		container: options.container || 'body',
+		responsive: options.responsive || false,
 		steps: options.steps,
 		rows: options.rows || 1,
 		width: options.width,
@@ -20,16 +23,35 @@ export default (target, options = {}) => {
 	 * Useful constants
 	 */
 
-	const windowsHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-	const uniqid = Symbol();
+	const windowsHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+	const uniqid = shortid.generate()
+
+
+	/**
+	 * Local variables
+	 */
+
+	let imageNode = null
+	let styleNode = null
+	let htmlNode  = null
+	let spriteColumns = null
+	let extraCSS = null
+	let backgroundSize = null
 
 
 	/**
 	 * Series of functions
 	 */
 
-	const run = [
+	const generate = [
+		_calculations,
+		_generateDOM,
 		_generateCSS
+	]
+
+	const run = [
+		load,
+		display
 	]
 
 
@@ -41,7 +63,7 @@ export default (target, options = {}) => {
 	const instance = knot({
 		init: init,
 		load: load,
-		unload: load,
+		display: display,
 		destroy: destroy,
 		listenScroll: listenScroll,
 		muteScroll: muteScroll,
@@ -59,34 +81,105 @@ export default (target, options = {}) => {
 
 	// Run a serie of functions
 	function _runSeries(functions) {
-		functions.forEach(func => func())
+		return new Promise(function(resolve, reject) {
+			resolve( functions.forEach(func => func()) )
+		})
 	}
 
-	// Generate the CSS steps
+	// Sprite calculations
+	function _calculations() {
+
+		// how many columns ?
+		spriteColumns = Math.ceil(settings.steps / settings.rows)
+
+		// what's background sizes
+		backgroundSize = 100 * spriteColumns
+
+		// fixed width calculation
+		let spriteWidth = settings.width / spriteColumns
+		spriteWidth = (Math.round((spriteWidth * 1000)/10)/100).toFixed(2)
+
+		// fixed height calculation
+		let spriteHeight = settings.height / settings.rows
+		spriteHeight = (Math.round((spriteHeight * 1000)/10)/100).toFixed(2)
+
+		// width = 100% & relative height
+		if (settings.responsive === true) {
+			let spritePadding = ((spriteHeight*100) * 100 / (spriteWidth*100))
+			extraCSS =
+				`
+				#sprite-${uniqid} {
+					position: relative;
+					width: 100%;
+				}
+
+				#sprite-${uniqid}::after {
+					content: '';
+  					display: block;
+  					padding-bottom: ${spritePadding}%;
+				}
+				`
+		// fixed sizes
+		} else {
+			extraCSS =
+				`
+				#sprite-${uniqid} {
+					position: absolute;
+					width: ${spriteWidth}px;
+					height: ${spriteHeight}px;
+				}
+				`
+		}
+	}
+
+	// Generate the DOM
+	function _generateDOM() {
+		if (htmlNode === null) {
+			htmlNode = document.createElement('div')
+			document.querySelector(settings.container).appendChild(htmlNode).setAttribute('id', 'sprite-'+ uniqid +'')
+		}
+	}
+
+	// Generate the CSS
 	function _generateCSS() {
+		if (styleNode === null && imageNode != null) {
 
-		console.log(uniqid);
+			// generate the css
+			let css =
+				`
+				#sprite-${uniqid} {
+					left: 0; right: 0; top: 0; bottom: 0;
+					background: url('${imageNode.src}') no-repeat 0 0%;
+					background-size: ${backgroundSize}%;
+					animation: sprite-anim-${uniqid} 4s steps(${spriteColumns}) infinite;
+				}
 
-		let css =
-		`
-		.sprite {
-			position: absolute;
-			left: 0;
-			right: 0;
-			top: 0;
-			bottom: 0;
-			background:  url('http://bennet.org/images/codepen/ryu-sprite-demo.png') no-repeat 0 0%;
-			background-size: 100%;
-			animation: sprite 3.5s steps(45) infinite;
+				${extraCSS}
+				`
+
+			//keyframes
+			// css += `@keyframes sprite-anim-${uniqid} {`
+			// css += `0% { background-position: 0 0; }`
+			// for (let i = 1; i <= settings.steps-1; i++) {
+			// 	let currPrct = (100 / (settings.steps-1)) * i
+			// 	let currPosX = (i % spriteColumns) * (backgroundSize / spriteColumns)
+			// 	let currPosY = (i % settings.rows) * (backgroundSize / settings.rows)
+			// 	css +=
+			// 		`
+			// 		${currPrct}% { background-position: ${currPosX}% ${currPosY}%; }
+			// 		`
+			// }
+			// css += `}`
+
+			// create style node
+			styleNode = document.createElement('style')
+			styleNode.innerHTML = css
+			styleNode.appendChild(document.createTextNode('')) // WebKit hack
+
+			// append style element to the head
+			document.head.appendChild(styleNode)
+
 		}
-
-		@keyframes sprite {
-			from { background-position: 0 0%; }
-			to { background-position: 0 100%; }
-		}
-		`
-
-
 	}
 
 
@@ -96,23 +189,34 @@ export default (target, options = {}) => {
 
 	// Init the instance
 	function init() {
-		_generateCSS()
-		return instance.emit('init')
+		_runSeries(run).then(function() {
+			return instance.emit('init')
+		})
 	}
 
-	// Load the sprite image
-	function load() {
-		return instance.emit('loaded')
-	}
-
-	// Unload the sprite from browser memory
-	function unload() {
-		return instance.emit('unloaded')
+	// Display the sprite
+	function display() {
+		_runSeries(generate).then(function() {
+			return instance.emit('display')
+		})
 	}
 
 	// Destroy completely the sprite and restore initial state
 	function destroy() {
+		imageNode = null
+		// TODO: Set background to 'none' & destroy dom
 		return instance.emit('destroy')
+	}
+
+	// Load the sprite image
+	function load() {
+		if (imageNode === null) {
+			imageNode = new Image()
+			imageNode.onload = function() {
+				return instance.emit('load')
+			}
+			imageNode.src = settings.src
+		}
 	}
 
 	// Listen for user scroll
@@ -127,11 +231,15 @@ export default (target, options = {}) => {
 
 	// Return the current frame/step
 	function getStep(step) {
-		return instance.emit('stepChanged')
+
 	}
 
 	// Change the current frame/step (no animation)
 	function setStep(step = 0) {
+		// 	let currPrct = (100 / (settings.steps-1)) * i
+		// 	let currPosX = (i % spriteColumns) * (backgroundSize / spriteColumns)
+		// 	let currPosY = (i % settings.rows) * (backgroundSize / settings.rows)
+		console.log(step)
 		return instance.emit('stepChanged')
 	}
 
